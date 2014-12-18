5dbaaedd89f13218f8dab000d2f14b5472e0a816
itomaldonado <mo.maldonado@gmail.com>
2014-12-18 14:33:44 -0500

    Modified app.py now it is a cyber app!

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print "hello, world!"
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def get_root():
+    return "Hello, cyberworld!"
+
+
+@app.route('/<name>', methods=['GET'])
+def get_name(name):
+    return "Hello, " + name + "!"
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=80, debug=True)
