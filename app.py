791e6d7af313758b243099cd9cdad69ef40a2db0
itomaldonado <mo.maldonado@gmail.com>
2014-12-22 11:17:14 -0500

    Pushing App.py and app_test.py with all my changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request, abort
 
 server = Flask(__name__)
 
@@ -11,6 +11,22 @@ def get_root():
 @server.route('/<name>', methods=['GET'])
 def get_name(name):
     return "Hello, " + name + "!"
+
+
+@server.route('/ovs/orders', methods=['POST'])
+def validate_order():
+    print "HEY: " + str(request.json)
+    if (request.json['address'] == ""):
+        return jsonify({'error': 'address field is empty'}), 400
+    if (request.json['name'] == ""):
+        return jsonify({'error': 'name field is empty'}), 400
+    if (request.json['productType'] == ""):
+        return jsonify({'error': 'orderType field is empty'}), 400
+    
+    
+    return str(request.json)
+    
+
 
 
 if __name__ == '__main__':
--- app_test.py
+++ app_test.py
@@ -12,6 +12,41 @@ class OVSTest(unittest.TestCase):
         result = self.applications.get('/' + name)
         assert "Hello, " + name + "!" == result.data
         
+    def test_order_validation(self):
+        order = {
+                 'name': 'manuel',
+                 'address': 'one vz way',
+                 'productType': 'Hey'
+                 }
+        result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
+        print result.status
+        assert "200" in result.status
+        
+        order = {
+                 'name': '',
+                 'address': 'one vz way',
+                 'productType': 'Hey'
+                 }
+        result = self.applications.post('/ovs/orders', data=order)
+        assert 400 == result.code and {'error': 'name is empty'} == result.data
+        
+        order = {
+                 'name': 'manuel',
+                 'address': '',
+                 'productType': 'Hey'
+                 }
+        result = self.applications.post('/ovs/orders', data=order)
+        assert 400 == result.code and {'error': 'address is empty'} == result.data
+        
+        order = {
+                 'name': 'manuel',
+                 'address': 'One VZ Way',
+                 'productType': ''
+                 }
+        result = self.applications.post('/ovs/orders', data=order)
+        assert 400 == result.code and {'error': 'productType is empty'} == result.data
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
